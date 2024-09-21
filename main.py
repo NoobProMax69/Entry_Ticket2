f1aa75b6c103cd07fcd50d3c7c1c4bbc965e1ed1
NoobProMax69 <chikichan18@yahoo.com>
2024-09-22 00:19:06 +0200

    Added code comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fileinput import filename
 
-
+#Function for printing the menu
 def print_menu():
     print("--------- Menu ----------")
     print("a) Add a new test subject")
@@ -10,36 +10,41 @@ def print_menu():
     print("q) Quit")
     print("-------------------------\n")
 
-def new_test_subject():
-    print("\n---- New test subject ----")
-    new_subject_number = input("Number: ")
-    new_subject_time = int(input("Time: "))
-    return new_subject_number, new_subject_time
 
+#Function for creating a new test subject
+def new_test_subject():
+    print("---- New test subject ----")
+    new_subject_number = input("Number: ")
+    new_subject_time = int(input("Time: "))         #takes the input as an integer
+    return new_subject_number, new_subject_time     #returning the subject name and time as a tuple
+
+#Function for writing data to a file
 def write_data(test_subjects, filename):
     with open(filename, "w") as file:
         for test_subjects in test_subjects:
-            file.write(test_subjects[0] + "\n")
-            file.write(str(test_subjects[1]) + "\n")
+            file.write(test_subjects[0] + "\n")         #Write the first element of the tuple
+            file.write(str(test_subjects[1]) + "\n")    #Write the second element of the tuple as a string
     print("*** Data written to file ***")
 
 
+#Function for reading data from a file
 def read_data(filename):
     test_subjects = []
     with open(filename, "r") as file:
         lines = file.readlines()
         for i in range(0, len(lines), 2):
             test_subject_number = lines[i].strip() #Remove newline character
-            test_subject_time = int(lines[i+1].strip())
+            test_subject_time = int(lines[i+1].strip()) #Convert string to integer and remove newline character
             test_subjects.append((test_subject_number, test_subject_time))
     print("*** Data read from file ***")
     return test_subjects
 
+#Function for printing the test subjects
 def print_test_subjects(test_subjects):
     print("\n---- Registered test subjects ----")
     for subject_number, subject_time in test_subjects:
-        print(f"Number : {subject_number:>5}")
-        print(f"Time   : {subject_time:>5}\n")
+        print(f"Number : {subject_number:>4}")
+        print(f"Time   : {subject_time:>4}\n")
 
 
 def main():
